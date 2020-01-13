bd2b438b5b29fcf00408b4e83f373e91ccc7463c
Mikhael D'Amato <damato578@gmail.com>
2020-01-13 09:24:41 -0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
